43a8a58c7864708ec5a8aa75c6619188627076ab
Justin Fay <justin.fay@hope.edu>
2023-08-18 17:06:35 -0400

    Initial Commit

---
+++ Barometer/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Barometer {
+    public partial class App : Application {
+        public App() {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Barometer/AppShell.xaml.cs
@@ -0,0 +1,7 @@
+﻿namespace Barometer {
+    public partial class AppShell : Shell {
+        public AppShell() {
+            InitializeComponent();
+        }
+    }
+}
